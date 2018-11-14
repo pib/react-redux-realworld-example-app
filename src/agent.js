a5c6a20519d9927ac2a3ad892ab34f2ed75d34d8
Paul Bonser <pib@paulbonser.com>
2018-11-14 02:19:26 -0600

    trailing slashes

--- src/agent.js
+++ src/agent.js
@@ -28,62 +28,62 @@ const requests = {
 
 const Auth = {
   current: () =>
-    requests.get('/user'),
+    requests.get('/user/'),
   login: (email, password) =>
-    requests.post('/users/login', { user: { email, password } }),
+    requests.post('/users/login/', { user: { email, password } }),
   register: (username, email, password) =>
-    requests.post('/users', { user: { username, email, password } }),
+    requests.post('/users/', { user: { username, email, password } }),
   save: user =>
-    requests.put('/user', { user })
+    requests.put('/user/', { user })
 };
 
 const Tags = {
-  getAll: () => requests.get('/tags')
+  getAll: () => requests.get('/tags/')
 };
 
 const limit = (count, p) => `limit=${count}&offset=${p ? p * count : 0}`;
 const omitSlug = article => Object.assign({}, article, { slug: undefined })
 const Articles = {
   all: page =>
-    requests.get(`/articles?${limit(10, page)}`),
+    requests.get(`/articles/?${limit(10, page)}`),
   byAuthor: (author, page) =>
-    requests.get(`/articles?author=${encode(author)}&${limit(5, page)}`),
+    requests.get(`/articles/?author=${encode(author)}&${limit(5, page)}`),
   byTag: (tag, page) =>
-    requests.get(`/articles?tag=${encode(tag)}&${limit(10, page)}`),
+    requests.get(`/articles/?tag=${encode(tag)}&${limit(10, page)}`),
   del: slug =>
-    requests.del(`/articles/${slug}`),
+    requests.del(`/articles/${slug}/`),
   favorite: slug =>
-    requests.post(`/articles/${slug}/favorite`),
+    requests.post(`/articles/${slug}/favorite/`),
   favoritedBy: (author, page) =>
-    requests.get(`/articles?favorited=${encode(author)}&${limit(5, page)}`),
+    requests.get(`/articles/?favorited=${encode(author)}&${limit(5, page)}`),
   feed: () =>
-    requests.get('/articles/feed?limit=10&offset=0'),
+    requests.get('/articles/feed/?limit=10&offset=0'),
   get: slug =>
-    requests.get(`/articles/${slug}`),
+    requests.get(`/articles/${slug}/`),
   unfavorite: slug =>
-    requests.del(`/articles/${slug}/favorite`),
+    requests.del(`/articles/${slug}/favorite/`),
   update: article =>
-    requests.put(`/articles/${article.slug}`, { article: omitSlug(article) }),
+    requests.put(`/articles/${article.slug}/`, { article: omitSlug(article) }),
   create: article =>
-    requests.post('/articles', { article })
+    requests.post('/articles/', { article })
 };
 
 const Comments = {
   create: (slug, comment) =>
-    requests.post(`/articles/${slug}/comments`, { comment }),
+    requests.post(`/articles/${slug}/comments/`, { comment }),
   delete: (slug, commentId) =>
-    requests.del(`/articles/${slug}/comments/${commentId}`),
+    requests.del(`/articles/${slug}/comments/${commentId}/`),
   forArticle: slug =>
-    requests.get(`/articles/${slug}/comments`)
+    requests.get(`/articles/${slug}/comments/`)
 };
 
 const Profile = {
   follow: username =>
-    requests.post(`/profiles/${username}/follow`),
+    requests.post(`/profiles/${username}/follow/`),
   get: username =>
-    requests.get(`/profiles/${username}`),
+    requests.get(`/profiles/${username}/`),
   unfollow: username =>
-    requests.del(`/profiles/${username}/follow`)
+    requests.del(`/profiles/${username}/follow/`)
 };
 
 export default {
